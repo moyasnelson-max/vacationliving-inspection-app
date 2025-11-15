ce1316ebb342a3285fb9dcfe3c6030dbe201de85
moyasnelson-max <moyasnelson@gmail.com>
2025-11-14 19:38:08 -0500

    Create page.tsx

---
+++ app/app/logout/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { useEffect } from "react";
+import { supabase } from "@/lib/supabaseClient";
+import { useRouter } from "next/navigation";
+
+export default function Logout() {
+  const router = useRouter();
+
+  useEffect(() => {
+    supabase.auth.signOut();
+    router.push("/login");
+  }, []);
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Signing out...</h2>
+    </div>
+  );
+}
