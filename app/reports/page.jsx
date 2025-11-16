5811dc82c0486bc6a3d747c3100f10abf7c7c86d
moyasnelson-max <moyasnelson@gmail.com>
2025-11-16 03:49:31 -0500

    Update page.jsx

--- app/reports/page.jsx
+++ app/reports/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-import as 'supabase';
+import supabase from "../lib/supabaseClient";
 import { useRouter } from "next/navigation";
 
 export default function ReportsPage() {
