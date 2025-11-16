65c5bb87f896b91a8704523b948b4323b444c9fe
moyasnelson-max <moyasnelson@gmail.com>
2025-11-16 02:53:22 -0500

    Update and rename page.tsx to page.jsx

--- app/logout/page.jsx
+++ app/logout/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect } from "react";
-import { supabase } from "../lib/supabaseClient";
+import supabase from "../lib/supabaseClient";
 import { useRouter } from "next/navigation";
 
 export default function Logout() {
