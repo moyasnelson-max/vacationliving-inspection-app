07ffe5f8d6a59d4b20034ba546d10b197f5687dd
moyasnelson-max <moyasnelson@gmail.com>
2025-11-16 05:11:59 -0500

    Update supabaseClient.js

--- app/lib/supabaseClient.js
+++ app/lib/supabaseClient.js
@@ -3,6 +3,17 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true,
+      storage: typeof window !== 'undefined' ? window.localStorage : undefined,
+    },
+  }
+);
 
 export default supabase;
