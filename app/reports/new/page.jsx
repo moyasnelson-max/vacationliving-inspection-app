8c6c412da2c3d339f78eff06cc683def06c09c13
moyasnelson-max <moyasnelson@gmail.com>
2025-11-19 12:37:35 -0500

    Update page.jsx

--- app/reports/new/page.jsx
+++ app/reports/new/page.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import supabase from "../../lib/supabaseClient";
+import supabase from "../../lib/supabase-client";
 import "../../../styles/glass.css";
 
 export default function NewReport() {
