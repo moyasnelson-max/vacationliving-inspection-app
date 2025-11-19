24bfae020422835695a24e5777dff071fd5fc058
moyasnelson-max <moyasnelson@gmail.com>
2025-11-19 12:40:20 -0500

    Update page.jsx

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,6 +1,5 @@
 "use client";
 import { useState } from "react";
-import { supabase } from "@/lib/supabase-client";
 import supabase from "../lib/supabase-client";
 
 export default function LoginPage() {
