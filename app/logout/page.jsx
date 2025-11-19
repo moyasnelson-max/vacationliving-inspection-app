36085e8df70422d8a757daffa180bd5d05d2fb21
moyasnelson-max <moyasnelson@gmail.com>
2025-11-19 12:35:06 -0500

    Update page.jsx

--- app/logout/page.jsx
+++ app/logout/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect } from "react";
-import supabase from "../lib/supabaseClient";
+import supabase from "../lib/supabase-client";
 import { useRouter } from "next/navigation";
 
 export default function Logout() {
