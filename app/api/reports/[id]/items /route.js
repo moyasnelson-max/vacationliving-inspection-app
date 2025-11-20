05ca7979e8f00ef45f567509183cb4b5d1456441
Nelson Moya <moyasnelson@gmail.com>
2025-11-20 15:25:35 -0500

    Update route.js

--- app/api/reports/[id]/items /route.js
+++ app/api/reports/[id]/items /route.js
@@ -1,4 +1,4 @@
-import supabase from "../../../../lib/supabase-client";
+import supabase from "../../../../lib/supabase-client.js";
 
 export async function GET(_, { params }) {
   const { id } = params;
