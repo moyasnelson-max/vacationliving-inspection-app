8ad85af8d4f9e44e1e8b4d690d7cc6b03c22fff9
Nelson Moya <moyasnelson@gmail.com>
2025-11-20 15:40:38 -0500

    Update route.js

--- app/api/items/[id]/delete/route.js
+++ app/api/items/[id]/delete/route.js
@@ -1,4 +1,4 @@
-import supabase from "../../../lib/supabase-client.js";
+import supabase from "@/app/lib/supabase-client.js";
 
 export async function DELETE(_, { params }) {
   const { id } = params;
