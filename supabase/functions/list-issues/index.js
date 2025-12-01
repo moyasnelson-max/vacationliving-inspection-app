47d99224075e50c8409aafa2fd67b6207ce27808
Digital Hiven <moyasnelson@gmail.com>
2025-12-01 16:19:55 -0500

    Create index.js

---
+++ supabase/functions/list-issues/index.js
@@ -0,0 +1,15 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export default async (_, res) => {
+  const supabase = createClient(
+    Deno.env.get("SUPABASE_URL"),
+    Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")
+  );
+
+  const { data } = await supabase
+    .from("issues")
+    .select("*, houses(name)")
+    .order("created_at", { ascending: false });
+
+  return res.json({ issues: data });
+};
