bcc8db5637bf3f7fde7915f8504e2539f351b87a
Digital Hiven <moyasnelson@gmail.com>
2025-12-01 15:19:32 -0500

    Create page.jsx

---
+++ app/inspection/[houseId]/submit/success/page.jsx
@@ -0,0 +1,8 @@
+export default function SubmitSuccess() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Inspection Submitted</h2>
+      <p>Your inspection has been successfully submitted.</p>
+    </div>
+  );
+}
