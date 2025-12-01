8f949cb9128bd53bd6ea461155a72c2d4dd8714f
Digital Hiven <moyasnelson@gmail.com>
2025-12-01 16:26:52 -0500

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,21 +1,11 @@
-self.addEventListener("install", (event) => {
+self.addEventListener("install", () => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener("activate", () => {
+  clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.open("offline-cache").then(async (cache) => {
-      try {
-        const response = await fetch(event.request);
-        cache.put(event.request, response.clone());
-        return response;
-      } catch (err) {
-        return cache.match(event.request) || Response.error();
-      }
-    }),
-  );
+  return event.respondWith(fetch(event.request));
 });
