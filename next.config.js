f8b6f12540809f893d52de37be5d41bca26403ca
Digital Hiven <moyasnelson@gmail.com>
2025-12-01 16:27:42 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,12 @@
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      "@": path.resolve(__dirname, "app"),
-      "@lib": path.resolve(__dirname, "app/lib")
-    };
-    return config;
+  reactStrictMode: false,
+  experimental: {
+    serverActions: true
   },
+  images: {
+    unoptimized: true
+  }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
