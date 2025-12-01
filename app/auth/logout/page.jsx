0eaeead7c2902674263f94d253e93f737cc431a0
Digital Hiven <moyasnelson@gmail.com>
2025-12-01 14:52:31 -0500

    Create page.jsx

---
+++ app/auth/logout/page.jsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { supabase } from "@/lib/supabase-client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function LogoutPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    async function doLogout() {
+      await supabase.auth.signOut();
+      router.push("/auth/login");
+    }
+    doLogout();
+  }, []);
+
+  return <p style={{ padding: 20 }}>Logging out...</p>;
+}
