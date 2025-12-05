03f6cef9d40cfca4a55c690a02b51be702f5765d
Nelson Moya <moyasnelson@gmail.com>
2025-12-05 05:16:47 +0000

    Fix absolute style imports for Vercel

--- app/components/LuxHeader.jsx
+++ app/components/LuxHeader.jsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import "@/styles/lux-header.css";
+import "../../styles/lux-header.css";
 
 export default function LuxHeader({ title = "Vacation Living", back }) {
   return (
